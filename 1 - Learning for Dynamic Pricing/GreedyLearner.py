f7d8c2e356b37cfbf570ee022819017cfa7109ac
Ozan İncesulu <ozyinc@users.noreply.github.com>
2022-04-29 19:16:53 +0200

    Simplify GreedyLearner.py to some extent

--- 1 - Learning for Dynamic Pricing/GreedyLearner.py
+++ 1 - Learning for Dynamic Pricing/GreedyLearner.py
@@ -1,8 +1,6 @@
-"""
-    Basic Learner.
-"""
+from collections import defaultdict
 from operator import itemgetter
-from typing import Tuple, Annotated, Optional
+from typing import Tuple, Optional
 
 from matplotlib import pyplot as plt
 from sklearn.tree import plot_tree
@@ -10,34 +8,6 @@ from Environment import Environment, constant_generator
 from Customer import CustomerClass, purchase_amounts, customer_counts, reservation_price_distribution_from_curves
 from Product import Product, ObservationProbability
 from sample import generate_sample_greedy_environment
-
-
-import numpy as np
-
-
-class Learner:
-    """
-        This is the same Learner of the prof
-    """
-
-    def __init__(self, n_arms):
-        """
-        :param n_arms:
-        """
-        self.n_arms = n_arms
-
-        self.t = 0
-        self.rewards_per_arm = [[] for _ in range(n_arms)]
-        self.collected_rewards = np.array([])
-
-    def update_observations(self, pulled_arm, reward):
-        """
-        :param pulled_arm:
-        :param reward:
-        :return:
-        """
-        self.rewards_per_arm[pulled_arm].append(reward)
-        self.collected_rewards = np.append(self.collected_rewards, reward)
 
 
 class GreedyLearner:
@@ -56,13 +26,15 @@ class GreedyLearner:
                 return 0
 
             product_price = product.candidate_prices[current_price_indexes[product.id]]
-            reservation_price = reservation_price_distribution_from_curves(class_, product.id, product_price).get_expectation()
+            reservation_price = reservation_price_distribution_from_curves(class_, product.id,
+                                                                           product_price).get_expectation()
             is_purchased = reservation_price >= product_price
             if not is_purchased:
                 return 0
             expected_purchase_count = purchase_amounts[class_][product.id].get_expectation()
-            result_ = (product_price - product.production_cost) * viewing_probability * n_users * expected_purchase_count
-            result_ = round(result_, 2) # 2 because we want cents :)
+            result_ = (
+                              product_price - product.production_cost) * viewing_probability * n_users * expected_purchase_count
+            result_ = round(result_, 2)  # 2 because we want cents :)
             first_p: Optional[ObservationProbability]
             second_p: Optional[ObservationProbability]
             first_p, second_p = product.secondary_products
@@ -70,7 +42,8 @@ class GreedyLearner:
             if first_p is not None:
                 result_ += emulate_path(new_primaries, first_p[1] * viewing_probability * 1, first_p[0])
             if second_p is not None:
-                result_ += emulate_path(new_primaries, second_p[1] * viewing_probability * self.env.lambda_, second_p[0])
+                result_ += emulate_path(new_primaries, second_p[1] * viewing_probability * self.env.lambda_,
+                                        second_p[0])
             return result_
 
         return emulate_path((), self.env.alpha[product.id + 1], product)
@@ -117,65 +90,43 @@ if __name__ == '__main__':
     learner = GreedyLearner(env)
     running = True
     cnt = 0
-    
+
     currentReward = []
-    currentCNT = []
-    p1 = []
-    p2 = []
-    p3 = []
-    p4 = []
-    p5 = []
-    
+    products = defaultdict(list)
     while running:
         running = learner.iterate_once()
         print(f"iteration {cnt}:")
         print("Indexes", learner.candidate_price_indexes)
         print("Reward", learner.current_reward)
         cnt += 1
-        
-        currentCNT.append(cnt)
+
         # Save the current reward
         currentReward.append(learner.current_reward)
         # Store the price indexes
-        p1.append(learner.candidate_price_indexes[0])
-        p2.append(learner.candidate_price_indexes[1])
-        p3.append(learner.candidate_price_indexes[2])
-        p4.append(learner.candidate_price_indexes[3])
-        p5.append(learner.candidate_price_indexes[4])        
-                
+        for product_i in range(5):
+            products[product_i].append(learner.candidate_price_indexes[product_i])
+
     print("###############################################\n")
     print("Done!")
     print("Identified price indexes:", learner.candidate_price_indexes)
-    
-    # Plot the current_reward over iterations    
+
+    currentCNT = list(range(1, cnt + 1))
+    # Plot the current_reward over iterations
     plt.plot(currentCNT, currentReward)
     plt.xlabel("Iteration")
     plt.ylabel("Reward")
     plt.title("Greedy Algorithm Reward")
     plt.show()
-    
+
     # Plot the prices p1, p2, p3, p4 and p5 over the iterations
-    priceP1 = []
-    priceP2 = []
-    priceP3 = []
-    priceP4 = []
-    pricep5 = []
-    
-    for i in range(len(currentCNT)):
-        priceP1.append(env.products[0].candidate_prices[p1[i]])
-        priceP2.append(env.products[1].candidate_prices[p2[i]])
-        priceP3.append(env.products[2].candidate_prices[p3[i]])
-        priceP4.append(env.products[3].candidate_prices[p4[i]])
-        pricep5.append(env.products[4].candidate_prices[p5[i]])
-                
-    plt.plot(currentCNT, priceP1, label="p1")
-    plt.plot(currentCNT, priceP2, label="p2")
-    plt.plot(currentCNT, priceP3, label="p3")
-    plt.plot(currentCNT, priceP4, label="p4")
-    plt.plot(currentCNT, pricep5, label="p5")
-    
+    prices = defaultdict(list)
+    for productId in range(5):
+        for i in range(cnt):
+            prices[productId].append(env.products[productId].candidate_prices[products[productId][i]])
+
+        plt.plot(currentCNT, prices[productId], label="p" + str(productId + 1))
     plt.xlabel("Iteration")
     plt.ylabel("Prices per product")
     plt.title("Greedy Algorithm Prices")
     plt.legend()
-    plt.show()    
+    plt.show()
