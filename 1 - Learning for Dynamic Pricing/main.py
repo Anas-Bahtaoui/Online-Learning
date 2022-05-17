53303271a2eb886615a265a9a1d8a3c3e1c4d572
Ozan İncesulu <ozyinc@users.noreply.github.com>
2022-05-17 20:28:43 +0200

    future plans

--- 1 - Learning for Dynamic Pricing/main.py
+++ 1 - Learning for Dynamic Pricing/main.py
@@ -1,9 +1,8 @@
 from typing import List
 
-from BanditLearner import step3
+from BanditLearner import step3, step4, step5, step6_sliding_window, step6_change_detection, step7
 from GreedyLearner import GreedyLearner
 from Learner import Learner
-from TSLearner import TSLearner
 from UCBLearner import UCBLearner
 from GaussianThompsonLearner import GaussianTSLearner
 from parameters import environment
@@ -12,10 +11,16 @@ if __name__ == '__main__':
 
     learners: List[Learner] = [
         GreedyLearner(),
+        # TODO: Remove after this line and use the lines below when all is done
+        # Better looking sexy code :)
+
         UCBLearner(step3),
-        # TSLearner(step3)
         GaussianTSLearner(step3)
     ]
+    # for step in [step3, step4, step5, step6_sliding_window, step6_change_detection, step7]
+    #     for Learner in [GaussianTSLearner, UCBLearner]:
+    #         learners.append(Learner(step))
+
     for learner in learners:
         environment.reset_day()
         learner.run_experiment(50, log=False, plot_graphs=True)
